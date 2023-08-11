4e07d1eb6e02b4bed5245f00c7f6e618de4f9ff6
adrianMartinezTeba <adrymate@gmail.com>
2023-08-11 21:27:50 +0200

    arreglando para que coja la inf del usuario conectado

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.post("/register", UserController.register);
 router.get("/confirmRegister/:emailToken", UserController.confirm);
 router.post("/login", UserController.login);
 router.delete("/logout",authentication, UserController.logout);
-router.get("/userInfo/:_id", UserController.getInfoById);
+router.get("/userInfo", authentication,UserController.getInfoById);
 router.get('/recoverPassword/:email', UserController.recoverPassword);
 router.put("/resetPassword/:recoverToken", UserController.resetPassword);
 
