ee7587bcad7aef5f6b392d5e9eaadb78c287eb08
adrianMartinezTeba <adrymate@gmail.com>
2023-08-11 18:29:06 +0200

    desplegado en railway y agregando correo de confirmacion

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
-const express = require("express")
-const UserController = require("../controllers/UserController")
-const router = express.Router()
+const express = require("express");
+const UserController = require("../controllers/UserController");
+const router = express.Router();
 
-router.post("/register",UserController.register)
-router.put("/confirmRegister/:_email", UserController.confirm)
-router.post("/login",UserController.login)
-router.delete("/logout", UserController.logout)
-router.get("/userInfo/:_id", UserController.getInfoById)
-router.get('/recoverPassword/:email',UserController.recoverPassword)
-router.put("/resetPassword/:recoverToken", UserController.resetPassword)
-module.exports = router
+router.post("/register", UserController.register);
+router.put("/confirmRegister/:emailToken", UserController.confirm);
+router.post("/login", UserController.login);
+router.delete("/logout", UserController.logout);
+router.get("/userInfo/:_id", UserController.getInfoById);
+router.get('/recoverPassword/:email', UserController.recoverPassword);
+router.put("/resetPassword/:recoverToken", UserController.resetPassword);
+
+module.exports = router;
