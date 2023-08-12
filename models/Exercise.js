350bb05b7364edf31570e60be6af50b4152a5e0d
adrianMartinezTeba <adrymate@gmail.com>
2023-08-12 16:07:33 +0200

    añadido a ejercicios zonas musculares

--- models/Exercise.js
+++ models/Exercise.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const ExerciseSchema = new mongoose.Schema({
   name: String,
   description: String,
-  type: String,
+  muscleZonePrincipal: String,
+  muscleZoneSecundaries: [{String}],
   difficulty: String,
   image: String,
   demoVideoLink: String,
