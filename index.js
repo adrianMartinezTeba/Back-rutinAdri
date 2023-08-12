51d2809072558dc40084e729daebf6c415e9fe1f
adrianMartinezTeba <adrymate@gmail.com>
2023-08-12 03:31:09 +0200

    instalado cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();//para poder usar dotenv
 const PORT = process.env.PORT || 3001;
 
 const { dbConnection } = require("./config/config")
 
 app.use(express.json())
-
+app.use(cors());
 dbConnection()
 
 app.use("/users", require("./routes/users"));
