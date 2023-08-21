c5b46663fed42ada3e4e5bcc59b438f4f5319724
adrianMartinezTeba <adrymate@gmail.com>
2023-08-21 12:33:39 +0200

    añadir controladores para buscador

--- routes/exercises.js
+++ routes/exercises.js
@@ -5,9 +5,9 @@ const router = express.Router()
 router.post("/create",ExerciseController.createExercise)
 router.delete("/delete/:_id",ExerciseController.deleteExercise)
 router.put("/update/:_id", ExerciseController.updateExercise)
+router.get('/all',ExerciseController.getExercises)
 router.get("/byId/:_id", ExerciseController.getExerciseById)
 router.get("/byName/:name", ExerciseController.getExerciseByName)
-router.get('/all',ExerciseController.getExercises)
 router.get("/byMuscleZonePrincipal/:muscleZone", ExerciseController.getExercisesByMuscleZonePrincipal);
 router.get("/byMuscleZoneSecundaries/:muscleZone", ExerciseController.getExercisesByMuscleZoneSecundaries);
 router.get("/byDifficulty/:difficulty", ExerciseController.getExercisesByDifficulty);
